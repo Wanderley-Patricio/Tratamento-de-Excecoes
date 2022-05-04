9d86f3d93444085dd0a6773c65f720b1bc44b378
Wanderley-Patricio <86197936+Wanderley-Patricio@users.noreply.github.com>
2022-05-03 23:37:29 -0300

    1ª versão - Tudo no programa principal

--- aplicacao/Programa.java
+++ aplicacao/Programa.java
@@ -26,9 +26,51 @@ DevSuperior
  */
 package aplicacao;
 
+import entidades.Conta;
+import java.util.Locale;
+import java.util.Scanner;
+
 public class Programa {
 
     public static void main(String[] args) {
-       
+      
+      Locale.setDefault(Locale.US);             // faz com que o programa entenda que o separador de decimal é ponto ao invés de vírgula.
+      Scanner scanner = new Scanner(System.in); // instanciando uma variável do tipo scanner.
+
+      System.out.println("Informe os dados da conta");
+      System.out.print("Número conta: ");
+      int cc = scanner.nextInt();            // após o enter o cursor ficará preso em uma linha vazia 
+      System.out.print("Titular: "); 
+      scanner.nextLine();                    // esse nextLine está consumindo o enter dado no nextInt.
+      String titular = scanner.nextLine();   //nextLine me permite ler nome com espaços. Lembrando que como foi utilizado o nextInt e apertei enter
+      System.out.print("Saldo inicial: ");   // esse enter ficar armazenado no buffer de entrada se eu der um único nextLine eu vou apenas consumir
+      Double saldo = scanner.nextDouble();   // o enter mas não vou ler nada, por esse motivo eu usei um scanner.nextLine antes desse.
+      System.out.print("Limite de saque: ");        
+      Double saque = scanner.nextDouble();
+      
+      Conta conta = new Conta(cc, titular, saldo, saque); // Ctrl + enter para completar 
+      
+      System.out.println("");
+      System.out.print("Informe uma quantia para sacar: ");
+      double valor = scanner.nextDouble();
+
+      if(valor > conta.getLimiteSaque()){
+          System.out.println("Erro de saque: A quantia excede o limite de saque.");      
+      }
+      else if(valor > conta.getSaldo()){
+          System.out.println("Saldo insuficiente.");
+      }
+      else{        
+            conta.sacar(valor);
+            System.out.printf("Novo saldo: %.2f", conta.getSaldo());
+            System.out.println("");
+      }
+      scanner.close();
     } 
 }
+
+/*Observação: Eu implementando tudo no programa principal eu não estou delegando as atividades, ou seja,
+um código que faz uma certa lógica deve estar na classe que implementa aquela lógica. Por exemplo, o 
+if e else utilizados no programa principal estão fazendo verificações que deveriam ser realizadas pela classe
+conta.
+*/
--- entidades/Conta.java
+++ entidades/Conta.java
@@ -1,5 +1,56 @@
 package entidades;
 
 public class Conta {
+    private Integer cc;
+    private String titular;
+    private Double saldo;
+    private Double limiteSaque;
+
+    public Conta(Integer cc, String titular, Double saldo, Double saque) {
+        this.cc = cc;
+        this.titular = titular;
+        this.saldo = saldo;
+        this.limiteSaque = saque;
+    }
+
+    public Integer getCc() {
+        return cc;
+    }
+
+    public void setCc(Integer cc) {
+        this.cc = cc;
+    }
+
+    public String getTitular() {
+        return titular;
+    }
+
+    public void setTitular(String titular) {
+        this.titular = titular;
+    }
+
+    public Double getSaldo() {
+        return saldo;
+    }
+
+    public void setSaldo(Double saldo) {
+        this.saldo = saldo;
+    }
+
+    public Double getLimiteSaque() {
+        return limiteSaque;
+    }
+
+    public void setLimiteSaque(Double saque) {
+        this.limiteSaque = saque;
+    }
     
+    public void depositar(Double valor){
+        saldo += valor;
+    }
+    
+    public void sacar(Double valor){
+        saldo -= valor;
+    }
+            
 }
