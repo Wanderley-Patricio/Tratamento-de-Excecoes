5edddd8c8ce61c2ddcccd1fb4a68300561c8fa86
Wanderley-Patricio <86197936+Wanderley-Patricio@users.noreply.github.com>
2022-05-03 22:31:11 -0300

    1º Commit

---
+++ aplicacao/Programa.java
@@ -0,0 +1,34 @@
+/*
+Agenda:
+• Estudo de caso: saque em conta bancária
+• Conceito
+• Delegação
+• Manutenqäo / código limpo
+• Soluqöes:
+    • 1) Tudo no programa principal
+    • 2) Função "gambiarra"
+    • 3) Exceções
+
+Exercício
+Fazer um programa para ler os dados de uma conta bancária e depois
+realizar um saque nesta conta bancária, mostrando o novo saldo. Um
+saque não pode ocorrer ou se não houver saldo na conta, ou se o valor
+do saque for superior ao limite de saque da conta. Implemente a conta
+bancária conforme projeto abaixo:
+
+Fonte:
+https://www.youtube.com/watch?v=AiIuJqJ0r8A
+https://github.com/acenelio/exceptions2-java
+Tratamento de exceções : aprenda na prática : exemplo Java
+DevSuperior
+
+@author Adaptado por Wanderley Patrício de Sousa Neto
+ */
+package aplicacao;
+
+public class Programa {
+
+    public static void main(String[] args) {
+       
+    } 
+}
